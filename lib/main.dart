006ab318f29babae9ac8f201ee6b41c46f1d9ac4
SHULK1033 <andres10333@hotmail.com>
2020-04-09 21:20:08 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:news_app/src/pages/tabs_page.dart';
 import 'package:news_app/src/services/news_services.dart';
 import 'package:news_app/src/theme/tema.dart';
-import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
